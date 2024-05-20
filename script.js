889c83318c938dd5d89b32f6f310ed4434a97fed
yesetoda <107122034+yesetoda@users.noreply.github.com>
2024-05-20 15:00:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 // Yenu and Yabu Work On the logic of the calculator here
+var display = document.getElementById('display');
+var operand1 = 0;
+var operand2 = null;
+var operator = null;
+
+function inputNumber(num) {
+    if (display.value === '0' || display.value === '') {
+        display.value = num;
+    } else {
+        display.value += num;
+    }
+}
+
+function inputDecimal() {
+    if (display.value.indexOf('.') === -1) {
+        display.value += '.';
+    }
+}
+
+function clearDisplay() {
+    display.value = '';
+    operand1 = 0;
+    operand2 = null;
+    operator = null;
+}
+
+function add() {
+    operand1 = display.value;
+    operator = '+';
+    display.value = '';
+}
 
